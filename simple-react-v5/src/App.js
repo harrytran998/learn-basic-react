ecfeb1df27690f0e03b8fbee4af718ff09967f13
NhatDeepTrai <demonslight998@gmail.com>
2019-08-29 16:02:09 +0700

    :sparkles: Reuseble components

--- simple-react-v5/src/App.js
+++ simple-react-v5/src/App.js
@@ -1,7 +1,18 @@
+const Pet = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Chihuahua'),
+    React.createElement('h2', {}, 'Chihuahua'),
+    React.createElement('h3', {}, 'Chihuahua')
+  ])
+}
+
 const App = () => {
   return React.createElement('div', { id: 'insert-someAtributes' }, [
     React.createElement('h1', {}, 'Hello bae'),
-    React.createElement('h1', {}, 'Hello bae')
+    React.createElement('h1', {}, 'Hello bae'),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
   ])
 }
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
