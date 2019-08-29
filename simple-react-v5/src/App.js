dd87181d8d1a7b6ded0ce57e728b3535a189c2ef
NhatDeepTrai <demonslight998@gmail.com>
2019-08-29 17:36:48 +0700

    :sparkles: Add react vs react-dom

--- simple-react-v5/src/App.js
+++ simple-react-v5/src/App.js
@@ -1,17 +1,21 @@
+import { createElement } from 'react'
+import { render } from 'react-dom'
+
 const Pet = ({ name, age, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, age),
-    React.createElement('h3', {}, breed)
+  return createElement('div', {}, [
+    createElement('h1', {}, name),
+    createElement('h2', {}, age),
+    createElement('h3', {}, breed)
   ])
 }
 
 const App = () => {
-  return React.createElement('div', { id: 'insert-someAtributes' }, [
-    React.createElement('h1', {}, 'Hello bae'),
-    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
-    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
-    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' })
+  return createElement('div', { id: 'insert-someAtributes' }, [
+    createElement('h1', {}, 'Hello bae'),
+    createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
+    createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
+    createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' })
   ])
 }
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+
+render(createElement(App), document.getElementById('root'))
