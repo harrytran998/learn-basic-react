824972c085720766d340cca575bece0babf65119
NhatDeepTrai <demonslight998@gmail.com>
2019-08-29 16:10:39 +0700

    :sparkles: Passing props

--- simple-react-v5/src/App.js
+++ simple-react-v5/src/App.js
@@ -1,18 +1,17 @@
-const Pet = () => {
+const Pet = props => {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Chihuahua'),
-    React.createElement('h2', {}, 'Chihuahua'),
-    React.createElement('h3', {}, 'Chihuahua')
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.age),
+    React.createElement('h3', {}, props.breed)
   ])
 }
 
 const App = () => {
   return React.createElement('div', { id: 'insert-someAtributes' }, [
     React.createElement('h1', {}, 'Hello bae'),
-    React.createElement('h1', {}, 'Hello bae'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
+    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' }),
+    React.createElement(Pet, { name: 'Fuck', age: 20, breed: 'Alient' })
   ])
 }
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
