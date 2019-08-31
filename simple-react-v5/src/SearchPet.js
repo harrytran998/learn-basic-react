4da34dec6c29f4a9c84ff7ef105c294edf6b3eef
NhatDeepTrai <demonslight998@gmail.com>
2019-08-31 10:42:51 +0700

    :sparkles: use Effect Hook

--- simple-react-v5/src/SearchPet.js
+++ simple-react-v5/src/SearchPet.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useDropDown from './UseDropDown'
-
-const Animal = ['Dog', 'Cat', 'Pig']
-
-const Pet = [
-  {
-    petName: 'A',
-    petAge: 1
-  },
-  {
-    petName: 'B',
-    petAge: 1
-  },
-  {
-    petName: 'C',
-    petAge: 1
-  }
-]
+import pet, { ANIMALS } from '@frontendmasters/pet'
 
 const SearchPet = () => {
-  const [petName, PetDropDown] = useDropDown('pet', 'dog', Animal)
+  const [location, setLocation] = useState('Seattle, WA')
+  const [breeds, setBreeds] = useState([])
+  const [breed, BreedDropDown, setBreed] = useDropDown('breed', '', breeds)
+  const [petName, PetDropDown] = useDropDown('pet', 'dog', ANIMALS)
+  useEffect(() => {
+    setBreeds([])
+    setBreed('')
+    pet
+      .breeds(petName)
+      .then(({ breeds }) => {
+        setBreeds(breeds.map(({ name }) => name))
+      })
+      .catch(err => console.log(err))
+  }, [petName])
   return (
     <div>
-      <h1>Hello Pet</h1>
-      <PetDropDown />
+      <div className="search-params">
+        <form>
+          <label htmlFor="location">
+            Location
+            <input className="ml-4" id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)} />
+          </label>
+          <br></br>
+          <PetDropDown />
+          <br></br>
+          <BreedDropDown />
+          <br></br>
+          <button className="btn btn-lg btn-primary my-4">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
